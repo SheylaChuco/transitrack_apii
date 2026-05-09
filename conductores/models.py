3e14ab8e8807f8a2cc063322795a1c6d06591c60
Sheyla <sheylachuco123@gmail.com>
2026-05-09 16:55:26 -0500

    create conductor and ruta models

--- conductores/models.py
+++ conductores/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
+class Conductor(models.Model):
+
+    nombre = models.CharField(max_length=100)
+
+    numero_licencia = models.CharField(
+        max_length=50,
+        unique=True
+    )
+
+    def __str__(self):
+        return self.nombre
 # Create your models here.
--- rutas/models.py
+++ rutas/models.py
@@ -1,3 +1,20 @@
 from django.db import models
+from conductores.models import Conductor
 
-# Create your models here.
+class Ruta(models.Model):
+
+    origen = models.CharField(max_length=100)
+
+    destino = models.CharField(max_length=100)
+
+    horario = models.CharField(max_length=50)
+
+    conductor = models.ForeignKey(
+        Conductor,
+        on_delete=models.CASCADE,
+        related_name='rutas'
+    )
+
+    def __str__(self):
+        return f"{self.origen} - {self.destino}"
+
